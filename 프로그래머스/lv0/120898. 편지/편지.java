9e3a2bfb018c7ac4092b1f62f95be89c40709f75
Moon's <118891828+mooooonmin@users.noreply.github.com>
2023-10-01 21:18:25 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120898. 편지/편지.java
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
         
-        answer = message.length() * 2;
-        return answer;
+        return message.length() * 2;
     }
 }
