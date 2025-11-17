1068878fd020d4e8330a492a089fde846894c729
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-17 15:21:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 248 ms, Memory: 17944 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+	
+	public static void main(String args[]){
+		
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++)
                 System.out.print("*");
-            }
-            System.out.println();
+                System.out.println();
         }
-    }
+	}
+	
 }
