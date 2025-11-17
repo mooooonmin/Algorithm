db5de8ff81a3555dc05dce1d7978c646e9cf300d
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-17 14:41:56 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17944 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        
-        System.out.println(A/B);
-    }
+	
+	public static void main(String args[]){
+		
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		System.out.println(a / b);
+	}
+	
 }
