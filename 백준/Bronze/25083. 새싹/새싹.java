ebd12825dc622cfc4344482af44ac960611f2419
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-18 14:09:01 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+public class Main {
+	
+	public static void main(String args[]){
+        // 첫 번째 줄: 공백 9개, 쉼표, 큰따옴표, 백슬래시, 큰따옴표, 숫자 7
+		System.out.println("         ,r'\"7"); 
+        // 두 번째 줄: r`-_   ,'  ,/ (마지막에 백슬래시가 없어 에러는 안 나지만, 모양 유지를 위해 공백을 맞춥니다.)
+		System.out.println("r`-_   ,'  ,/");
+        // 세 번째 줄: 백슬래시, 마침표, 큰따옴표, 공백, L_r, 작은따옴표
+		System.out.println(" \\. \". L_r'");
+        // 네 번째 줄: 공백 3개, 백슬래시, 물결표, 백슬래시, /
+		System.out.println("   `~\\/");
+        // 다섯 번째 줄: 공백 6개, 파이프(|)
+		System.out.println("      |");
+        // 여섯 번째 줄: 공백 6개, 파이프(|)
+		System.out.println("      |");
+	}
+	
+}
