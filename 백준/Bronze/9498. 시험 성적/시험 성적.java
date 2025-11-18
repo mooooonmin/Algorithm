a8bac563c0b26d17006c6170a2eadd4879f4a4e5
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-18 14:01:15 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,17 +1,30 @@
-import java.util.Scanner;
-public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int A = in.nextInt();
-		in.close();
-		
-		if(A>=90) System.out.println("A");
-		else if(A>=80) System.out.println("B");
-		else if (A>=70) System.out.println("C");
-		else if(A>=60) System.out.println("D");
-		else System.out.println("F");
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String args[]){
+		// 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+
+        if (a >= 90) {
+            System.out.println("A");
+
+        } else if(a >= 80) {
+            System.out.println("B");
+
+        } else if(a >= 70) {
+            System.out.println("C");
+
+        } else if(a >= 60) {
+            System.out.println("D");
+            
+        } else {
+            System.out.println("F");
+        }
+
+        sc.close(); 
 	}
- 
+	
 }
