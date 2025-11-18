45fc9f32d13926f862b40039c27eaaa3e6da4bfd
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-18 14:07:56 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13868 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,9 +1,16 @@
-public class Main {
-	public static void main(String[] args) {
+public class Main{
+	
+	public static void main(String args[]){
+        // 첫 번째 라인: |\ (백슬래시) _ / (백슬래시) |
 		System.out.println("|\\_/|");
+        // 두 번째 라인: | q p |   } / (백슬래시)
 		System.out.println("|q p|   /}");
+        // 세 번째 라인: ( 0 ) """ (백슬래시)
 		System.out.println("( 0 )\"\"\"\\");
+        // 네 번째 라인: | " ^ " `    |
 		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\\\__|");     
+        // 다섯 번째 라인: | | (백슬래시) _ / = \ (백슬래시) \ (백슬래시) | _ |
+		System.out.println("||_/=\\\\__|");
 	}
+	
 }
