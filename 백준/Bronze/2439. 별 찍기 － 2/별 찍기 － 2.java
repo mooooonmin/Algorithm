c32e7e45ebcaeb98ee59569bc1b43d8d3b754833
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-19 14:45:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 288 ms, Memory: 19240 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,18 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    
     public static void main(String[] args) {
-
+        
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+        int N = sc.nextInt(); 
+        sc.close();
+        
+        // 1. N번 반복하면서 각 줄을 처리하는 외부 반복문 (i는 현재 줄 번호)
+        for (int i = 1; i <= N; i++) {
+            
+            // 2. 공백을 출력하는 내부 반복문
+            // 총 공백 개수: N - i
+            for (int j = 1; j <= N - i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k < i; k++) {
+            
+            // 3. 별을 출력하는 내부 반복문
+            // 총 별 개수: i
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
+            
+            // 4. 한 줄의 출력이 끝났으므로 줄바꿈
             System.out.println();
         }
     }
