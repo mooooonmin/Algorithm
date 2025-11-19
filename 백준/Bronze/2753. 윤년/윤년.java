05740b2537e133de8c8484994f438e38ea3c9415
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-11-19 13:58:14 +0900

    [Bronze V] Title: 윤년, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,30 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String args[]){
 		
-		int y = in.nextInt();
-		in.close();
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+
+        if (a % 4 == 0) {
+            
+            if (a % 100 != 0) {
+                System.out.println(1); // 4의 배수이지만 100의 배수가 아님 (윤년)
+                
+            } else { // 4의 배수이면서 100의 배수인 경우
+                if (a % 400 == 0) {
+                    System.out.println(1); // 400의 배수임 (윤년)
+                } else {
+                    System.out.println(0); // 100의 배수이지만 400의 배수는 아님 (평년)
+                }
+            }
+            
+        } else {
+            System.out.println(0); // 4의 배수가 아님 (평년)
+        }
         
-		if(y%4==0) {
-			if(y%400==0) System.out.println("1");
-			else if(y%100==0) System.out.println("0");
-			else System.out.println("1");
-		}
-		else System.out.println("0");
 	}
+	
 }
