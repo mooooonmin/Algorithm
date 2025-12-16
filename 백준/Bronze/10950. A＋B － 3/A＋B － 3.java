5877853cf4a1a75d154df28dbdb0512abd80de99
Moon_minhee <118891828+mooooonmin@users.noreply.github.com>
2025-12-16 15:14:58 +0900

    [Bronze V] Title: A+B - 3, Time: 200 ms, Memory: 17908 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner sc =  new Scanner(System.in);
+        int T = sc.nextInt();
 
-        int c = sc.nextInt();
-        int arr[] = new int[c];
-
-        for (int i =0; i < c; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            arr[i] = a + b;
-        }
-
-        for (int k : arr) {
-            System.out.println(k);
+        for(int i=0; i < T; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A+B);
         }
     }
 }
